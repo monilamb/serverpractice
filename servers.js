628193475418f9f72d363308f0454c441a511105
MoniLamb <monica.lambrano@hotmail.com>
2018-08-07 19:34:31 -0400

    Fixed the code

--- servers.js
+++ servers.js
@@ -6,24 +6,32 @@ var PORT1 = 7000;
 var PORT2 = 7500;
 
 // Create a generic function to handle requests and responses
-function handleRequest(request, response) {
+function handleRequest1(request, response) {
 
   // Send the below string to the client when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
+  response.end("You good ");
 }
+
+function handleRequest2(request, response) {
+
+    // Send the below string to the client when the user visits the PORT URL
+    response.end("You bad");
+  }
 
 // Use the Node HTTP package to create our server.
 // Pass the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
+var server1 = http.createServer(handleRequest1);
+
+var server2 = http.createServer(handleRequest2);
 
 // Start our server so that it can begin listening to client requests.
-server.listen(PORT1, function() {
+server1.listen(PORT1, function() {
 
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT1);
 });
 
-server.listen(PORT2, function() {
+server2.listen(PORT2, function() {
 
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT2);
